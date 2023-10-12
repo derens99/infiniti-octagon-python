a6599b5a2219abe678e896e14babf98c24f520d4
Deren <derens99@gmail.com>
2023-10-12 00:26:57 -0400

    Documented Main class

--- infiniti_api/InfinitiAPI.py
+++ infiniti_api/InfinitiAPI.py
@@ -14,6 +14,10 @@ class InfinitiAPI:
     """
     A class representing the Infiniti API.
 
+    This class provides a consolidated interface for interacting with various endpoints
+    of the Infiniti Electro Optics' Octagon API. It initializes different endpoints like system,
+    device, pantilt, and visible lens, offering a streamlined API interaction experience.
+
     Attributes:
     -----------
     api_caller : RWYAPICaller
@@ -28,7 +32,30 @@ class InfinitiAPI:
         An instance of the VisibleLensEndpoint class.
     """
 
-    def __init__(self, base_url: str, auth: Tuple[str, str]):
+    def __init__(self, base_url: str, auth: Tuple[str, str]) -> None:
+        """
+        Initializes a new instance of the InfinitiAPI class.
+
+        Parameters:
+        -----------
+        base_url : str
+            The base URL of the Infiniti Electro Optics' Octagon API.
+        auth : Tuple[str, str]
+            A tuple containing the username and password for authentication.
+
+        Attributes Initialized:
+        -----------------------
+        api_caller : RWYAPICaller
+            A helper to make raw API calls.
+        system : SystemEndpoint
+            Endpoint object to interact with system-related functionalities.
+        device : DeviceEndpoint
+            Endpoint object to interact with device-related functionalities.
+        pantilt : PanTiltEndpoint
+            Endpoint object to interact with pan-tilt functionalities.
+        visible : VisibleLensEndpoint
+            Endpoint object to interact with visible lens functionalities.
+        """
         self.api_caller = RWYAPICaller(base_url, auth)
         self.system = SystemEndpoint(self.api_caller)
         self.device = DeviceEndpoint(self.api_caller)
@@ -36,6 +63,11 @@ class InfinitiAPI:
         self.visible = VisibleLensEndpoint(self.api_caller)
 
 if __name__ == '__main__':
+    # Instantiate the InfinitiAPI using environment variables for host and authentication
     api = InfinitiAPI(os.environ.get('host'), auth=(os.environ.get('user'), os.environ.get('password')))
+
+    # Fetch and print system versions using the instantiated API
     print(api.system.get_versions())
+
+    # Close the API caller's session
     api.api_caller.session.close()
